8b2eedc75d1c66a720c2af3b881f8ec9af85a109
tush87 <tusharj8788@gmail.com>
2023-02-15 17:59:36 +0530

    add mongoose

---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+mongoose.set('strictQuery',false);
+
+mongoose.connect('mongodb://localhost/Depo24)');
+
+const db=mongoose.connection;
+
+db.on('error',()=>{console.log("Error in connect DB")});
+
+db.once('open',()=>{console.log("Successfully connected to DB")});
+
+module.exports=db;
