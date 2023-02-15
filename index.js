58db52006ce4a5046b9314a662d08c85f3ad4bbd
tush87 <tusharj8788@gmail.com>
2023-02-15 17:55:05 +0530

    add layout

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const bodyParser=require('body-parser');
+const expressLayout=require('express-ejs-layouts');
 const port = 8000;
 
 const app=express();
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(expressLayout);
+
+
+app.use('/',require('./routes/index.js'));
 
 
 app.listen(port,function(err){
